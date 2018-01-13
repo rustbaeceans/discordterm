6279f11c02af0eb45e0c3a9f98974fb36d86f068
Em Sal <absolutezero2a03@gmail.com>
2018-01-13 15:23:09 -0800

    Initial event loop setup (press q to quit)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,77 @@
+#[macro_use]
 extern crate chan;
 extern crate termion;
 extern crate tui;
+
+use std::thread;
+use std::sync::{Arc, Mutex};
+
+use std::io;
 
 use termion::event;
 use termion::input::TermRead;
 
 use tui::Terminal;
-use tui::backend::TermionBackend;
+use tui::backend::RawBackend;
 use tui::widgets::{Widget, Block, Borders, Paragraph};
+use tui::layout::{Group, Size, Direction};
+
+struct AppState {
+
+}
 
 fn main() {
 
-    println!("Hello world!");
+    let backend = RawBackend::new().unwrap();
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    let terminal = Arc::new(Mutex::new(terminal));
+
+    let term = Arc::clone(&terminal);
+    term.lock().unwrap().clear().unwrap();
+    draw(&mut term.lock().unwrap());
+
+    let stdin = io::stdin();
+    let (tx, rx) = chan::sync(0);
+
+    let term = Arc::clone(&terminal);
+    thread::spawn(move || {
+        let tx = tx.clone();
+
+        for c in stdin.keys() {
+            let mut terminal = term.lock().unwrap();
+
+            let evt = c.unwrap();
+            if evt == event::Key::Char('q') {
+                tx.send(true);
+                break;
+            }
+            draw(&mut terminal);
+        }
+    });
+
+    loop {
+        chan_select! {
+            default => {},
+            rx.recv() => {
+                break;
+            }
+        }
+    }
+    let term = Arc::clone(&terminal);
+    let mut t = term.lock().unwrap();
+    t.show_cursor().unwrap();
+    t.clear().unwrap();
+
+}
+
+fn draw(t: &mut Terminal<RawBackend>) {
+    let size = t.size().unwrap();
+
+    Paragraph::default()
+        .text("Block")
+        .block(Block::default().borders(Borders::ALL).title("Terminal"))
+        .render(t, &size);
+
+    t.draw();
 }
