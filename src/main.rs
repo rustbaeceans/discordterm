f25141089b8e2b4dca08abeae9ef8d4ae3dd83f6
Em Sal <absolutezero2a03@gmail.com>
2018-01-13 14:42:32 -0800

    Setup

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate chan;
+extern crate termion;
+extern crate tui;
+
+use termion::event;
+use termion::input::TermRead;
+
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use tui::widgets::{Widget, Block, Borders, Paragraph};
+
+fn main() {
+
+    println!("Hello world!");
+}
