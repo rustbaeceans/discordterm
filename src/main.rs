c6c48632c6a6984cab52eb7b146c4bb7ee946d25
Ena Krdzalic <enakrdzalic@gmail.com>
2018-01-13 17:38:25 -0800

    User input

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,15 @@ use tui::widgets::{Widget, Block, Borders, Paragraph};
 use tui::layout::{Group, Size, Direction};
 
 struct AppState {
+    content: String,
+}
 
+impl AppState {
+    fn add_character(&mut self, chr: char) {
+        let mut content_to_append = String::new();
+        content_to_append.push(chr);
+        self.content = format!("{}{}", self.content, content_to_append);
+    }
 }
 
 fn main() {
@@ -25,28 +33,45 @@ fn main() {
     let backend = RawBackend::new().unwrap();
     let mut terminal = Terminal::new(backend).unwrap();
 
+    let mut app_state = AppState {
+        content: String::from(""),
+    };
+
     let terminal = Arc::new(Mutex::new(terminal));
+    let app_state = Arc::new(Mutex::new(app_state));
+
 
     let term = Arc::clone(&terminal);
+    let state = Arc::clone(&app_state);
+
     term.lock().unwrap().clear().unwrap();
-    draw(&mut term.lock().unwrap());
+    draw(&mut term.lock().unwrap(), &mut state.lock().unwrap());
 
     let stdin = io::stdin();
     let (tx, rx) = chan::async();
 
     let term = Arc::clone(&terminal);
+    let state = Arc::clone(&state);
     thread::spawn(move || {
         let tx = tx.clone();
 
         for c in stdin.keys() {
             let mut terminal = term.lock().unwrap();
+            let mut app_state = state.lock().unwrap();
 
             let evt = c.unwrap();
-            if evt == event::Key::Char('q') {
-                tx.send(true);
-                break;
+            match evt {
+                event::Key::Char(chr) => {
+                    app_state.add_character(chr);
+                },
+                event::Key::Ctrl('c') => {
+                    tx.send(true);
+                    break;
+                },
+                _ => {},
             }
-            draw(&mut terminal);
+            
+            draw(&mut terminal, &mut app_state);
         }
     });
 
@@ -65,11 +90,12 @@ fn main() {
 
 }
 
-fn draw(t: &mut Terminal<RawBackend>) {
+fn draw(t: &mut Terminal<RawBackend>, state: &mut AppState) {
     let size = t.size().unwrap();
+    let state = &*state;
 
     Paragraph::default()
-        .text("Block")
+        .text(&state.content[..])
         .block(Block::default().borders(Borders::ALL).title("Terminal"))
         .render(t, &size);
 
